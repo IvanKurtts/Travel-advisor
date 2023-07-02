fea2af65026171169d55f43f3335d51b5b3f9bce
IvanKurtts <ivankurt96@gmail.com>
2023-07-02 17:12:14 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { List } from "./components/List/List";
+import { Map } from "./components/Map/Map";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./api";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) =>
+        setCoordinates({ lat: latitude, lng: longitude })
+    );
+  }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+      setPlaces(data);
+      setIsLoading(false);
+    });
+  }, [bounds, coordinates]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw?.lat,
+        tr_latitude: ne?.lat,
+        bl_longitude: sw?.lng,
+        tr_longitude: ne?.lng,
+      },
+      headers: {
+        "X-RapidAPI-Key": "85f4e26daemsh6892fc40abd8290p14da76jsn9d6733fd49ca",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
